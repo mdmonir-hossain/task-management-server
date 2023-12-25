928c2d33f07e57b9225a497fecd26679c5cc42d0
mdmonir-hossain <monirsoftengr@gmail.com>
2023-12-25 23:22:06 +0600

    Dashboard

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -35,6 +35,12 @@ async function run() {
       res.send(result);
     });
 
+app.delete("/addtask/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await tasksCollection.deleteOne(query);
+  res.send(result);
+});
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
